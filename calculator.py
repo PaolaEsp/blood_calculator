b5d82f2819a394a5f08baa169228a1c8f28d48bc
Paola Espinal <Paola@Paolas-MacBook-Pro-2.local>
2020-08-24 13:05:26 -0400

    User can enter HDL test result

--- calculator.py
+++ calculator.py
@@ -10,5 +10,18 @@ def interface():
         elif choice == '1':
             HDL_driver()
 
+def HDL_driver():
+    # Get input
+    result = get_input()
+    def user_input():
+        choice1 = input("Enter your choice: ")
+        return int(HDL_input())
+
+    # Check if HDL is normal
+    def normality():
+
+    # Output
+
+
 
 interface()
