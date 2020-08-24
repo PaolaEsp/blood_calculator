8d5db5587d64fac3d2969d5b60c72c33b6d78bfa
Paola Espinal <Paola@Paolas-MacBook-Pro-2.local>
2020-08-24 17:23:45 -0400

    Analyze and output HDL levels and analysis

--- calculator.py
+++ calculator.py
@@ -14,23 +14,23 @@ def HDL_driver():
     # Get input
     HDL_result = get_input()
     HDL_analysis = analyze_HDL_result(HDL_result)
-    def get_input():
-        HDL_input = input("Enter your choice: ")
-        return int(HDL_input())
+    output_HDL_analysis(HDL_result, HDL_analysis)
+def get_input():
+    HDL_input = input("Enter the HDL test result: ")
+    return int(HDL_input)
 
     # Check if HDL is normal
-    def analyze_HDL_result(HDL_test_value):
-        if HDL_test_value >= 60:
-            return "Normal"
-        elif 40 <= HDL_test_value < 60:
-            return "Borderline Low"
-        else:
-            return "Low"
+def analyze_HDL_result(HDL_test_value):
+    if HDL_test_value >= 60:
+        return "Normal"
+    elif 40 <= HDL_test_value < 60:
+        return "Borderline Low"
+    else:
+        return "Low"
 
     # Output
-
-
-
-
+def output_HDL_analysis(test_result, analysis):
+    print("The HDL result is {}".format(test_result))
+    print("That is {}".format(analysis))
 
 interface()
